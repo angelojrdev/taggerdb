a3b0d6918c6765eca20cd7e051e2402f9d840eb6
Angelo Oliveira Jr. <43245439+angelojrdev@users.noreply.github.com>
2024-08-30 10:55:11 -0300

    PyPI test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="taggerdb",
-    version="0.1.0",
+    version="0.1.1",
     author="Angelo Oliveira Jr.",
     author_email="43245439+angelojrdev@users.noreply.github.com",
     description="A user-friendly tool designed for managing and tagging files",
