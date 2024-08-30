ae06fb2fba64a262c600e553cba728d6a02a1a27
Angelo Oliveira Jr. <43245439+angelojrdev@users.noreply.github.com>
2024-08-30 19:09:01 -0300

    Update logic for OOP usage

--- taggerdb/taggerdb.py
+++ taggerdb/taggerdb.py
@@ -6,76 +6,108 @@ import hashlib
 import argparse
 
 
-def create_tables(conn):
-    cursor = conn.cursor()
+class TaggerDb:
+    def __init__(self, database, storage, quickstart=False):
+        self.database = database
+        self.storage = storage
 
-    cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS files (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            location TEXT NOT NULL,
-            sha256 TEXT NOT NULL,
-            size INTEGER NOT NULL
-        )
-    """
-    )
+        if quickstart:
+            self.connect()
+            self.prepare_database()
 
-    cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS tags (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL UNIQUE,
-            description TEXT
-        )
-    """
-    )
+    def connect(self):
+        try:
+            if self.is_connected():
+                raise Exception("Database is connected already.")
 
-    cursor.execute(
-        """
-        CREATE TABLE IF NOT EXISTS file_tags (
-            file_id INTEGER,
-            tag_id INTEGER,
-            PRIMARY KEY (file_id, tag_id),
-            FOREIGN KEY (file_id) REFERENCES files(id) ON DELETE CASCADE,
-            FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE
-        )
-    """
-    )
+            self.connection = sqlite3.connect(self.database)
+            print("Info: Database connected.")
+        except Exception as e:
+            print(f"Error: {e}")
 
-    conn.commit()
+    def disconnect(self):
+        try:
+            if not self.is_connected():
+                raise Exception("Database isn't connected.")
 
+            self.connection.close()
+            print("Info: Database disconnected.")
+        except Exception as e:
+            print(f"Error: {e}")
 
-def scan_directory_and_insert(conn, dir_path):
-    cursor = conn.cursor()
-    for root, dirs, files in os.walk(dir_path):
-        for file in files:
-            file_path = os.path.join(root, file)
-            relative_path = os.path.relpath(file_path, dir_path)
-            file_size = os.path.getsize(file_path)
+    def is_connected(self):
+        try:
+            self.connection.execute("SELECT 1")
+            return True
+        except Exception:
+            return False
 
-            hash_obj = hashlib.new("sha256")
-            with open(file_path, "rb") as f:
-                for chunk in iter(lambda: f.read(4096), b""):
-                    hash_obj.update(chunk)
-            file_sha256 = hash_obj.hexdigest()
+    def prepare_database(self):
+        try:
+            if not self.is_connected():
+                raise Exception("Database isn't connected.")
+
+            cursor = self.connection.cursor()
 
             cursor.execute(
-                """
-                SELECT 1 FROM files WHERE sha256 = ? AND size = ?
-            """,
-                (file_sha256, file_size),
+                """ CREATE TABLE IF NOT EXISTS file (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    location TEXT NOT NULL,
+                    sha256 TEXT NOT NULL,
+                    size INTEGER NOT NULL )"""
             )
-            if cursor.fetchone() is None:
+
+            cursor.execute(
+                """ CREATE TABLE IF NOT EXISTS tag (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL UNIQUE )"""
+            )
+
+            cursor.execute(
+                """ CREATE TABLE IF NOT EXISTS tag_x_file (
+                    file INTEGER,
+                    tag INTEGER,
+                    PRIMARY KEY (file, tag),
+                    FOREIGN KEY (file) REFERENCES file(id) ON DELETE CASCADE,
+                    FOREIGN KEY (tag) REFERENCES tag(id) ON DELETE CASCADE )"""
+            )
+
+            self.connection.commit()
+            print("Info: Database is now ready.")
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def scan(self):
+        cursor = self.connection.cursor()
+        for root, _, files in os.walk(self.storage):
+            for file in files:
+                file_path = os.path.join(root, file)
+                relative_path = os.path.relpath(file_path, self.storage)
+                file_size = os.path.getsize(file_path)
+
+                hash_obj = hashlib.new("sha256")
+                with open(file_path, "rb") as f:
+                    for chunk in iter(lambda: f.read(4096), b""):
+                        hash_obj.update(chunk)
+                file_sha256 = hash_obj.hexdigest()
+
                 cursor.execute(
-                    """
-                    INSERT INTO files (location, sha256, size)
-                    VALUES (?, ?, ?)
-                """,
+                    "SELECT 1 FROM file WHERE sha256 = ? AND size = ?",
+                    (file_sha256, file_size),
+                )
+
+                if cursor.fetchone() is not None:
+                    continue
+
+                cursor.execute(
+                    """ INSERT INTO file (location, sha256, size)
+                    VALUES (?, ?, ?) """,
                     (relative_path, file_sha256, file_size),
                 )
                 print(f'Info: Added File "{relative_path}"')
 
-    conn.commit()
+        self.connection.commit()
+        print("Info: Done!")
 
 
 def add_tags(conn, file_id, tags):
@@ -154,26 +186,13 @@ def parse_arguments():
 
 def main():
     arguments = parse_arguments()
+    taggerdb = TaggerDb(arguments.database, arguments.directory, True)
 
     match arguments.action:
-        case "init":
-            if os.path.exists(arguments.database):
-                print(f"Error: {arguments.database} already exists.")
-                exit(1)
-
-            conn = sqlite3.connect(arguments.database)
-            create_tables(conn)
-            conn.close()
-            print("Info: Database was initialized successfully!")
         case "scan":
-            if not os.path.exists(arguments.database):
-                print(f"Error: {arguments.database} doesn't exist.")
-                exit(1)
-
-            conn = sqlite3.connect(arguments.database)
-            scan_directory_and_insert(conn, arguments.directory)
-            print("Info: Done!")
-            conn.close()
+            taggerdb.scan()
+    
+    taggerdb.disconnect()
 
     # if arguments.tags:
     #     cursor = conn.cursor()
